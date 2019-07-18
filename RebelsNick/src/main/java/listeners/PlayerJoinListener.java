dcad5ea0a21cda15364b88f4b602672754682d83
DESKTOP-V7JME9U\Simon <i360089@student.fontys.nl>
2019-07-18 17:03:19 +0200

    RebelsNick - finished

--- RebelsNick/src/main/java/listeners/PlayerJoinListener.java
+++ RebelsNick/src/main/java/listeners/PlayerJoinListener.java
@@ -7,14 +7,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import wailord2.rebelsnick.rebelsnick.RebelsNick;
 
+import java.util.concurrent.ExecutionException;
+
 import static org.bukkit.Bukkit.getLogger;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event){
-        getLogger().info("found player name: "  + RebelsNick.config.get(event.getPlayer().getUniqueId().toString()));
-        Player player = event.getPlayer();
-        player.setDisplayName(RebelsNick.config.get(player.getUniqueId().toString()).toString());
+        try{
+            getLogger().info("found player name: "  + RebelsNick.config.get(event.getPlayer().getUniqueId().toString()));
+            Player player = event.getPlayer();
+            player.setDisplayName(RebelsNick.config.get(player.getUniqueId().toString()).toString());
+        }catch (Exception e){
+            getLogger().info("RebelsNick - This player has not had their name changed yet, so no nickname could be applied.");
+        }
     }
 }
